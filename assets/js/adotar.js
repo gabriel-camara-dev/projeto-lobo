0e0c968efadf05645fe9f87a2cb0eee178cdb5ce
William Rocha <williammrochag@gmail.com>
2024-07-30 01:08:18 -0300

    feat(adotar_lobinho): recebe o nome e o id do lobo pro títutlo

--- assets/js/adotar.js
+++ assets/js/adotar.js
@@ -1,10 +1,34 @@
+function enviarDados() {
+        let nome = document.getElementById("form_nome").value;
+        let idade = document.getElementById("form_idade").value;
+        let email = document.getElementById("form_email").value;
+        let enviar = confirm('Tem certeza que deseja adotar o lobinho?');
+        if (enviar) {
+            loboAdotado();
+            alert("O lobinho foi adotado com sucesso!");
+        } else {
+            alert("O lobinho não foi adotado.");
+        }
+};
+
+function loboAdotado() {
+    fetch('assets/js/lobinhos.json')
+  .then(response => response.json())
+  .then(data => {
+    // 'data' é um array de objetos, escreve tudo aqui dentro
+    console.log(data);
+  })
+  .catch(error => console.error('Erro ao carregar o JSON:', error));
+}
+
 window.addEventListener('load', () => {
     const lobo = JSON.parse(localStorage.getItem('loboId'))
 
-    const lobo_nome = document.querySelector('.nome_lobo')
+    var lobo_nome = document.querySelector('.nome_lobo')
     lobo_nome.textContent = "Adotar o(a) " + lobo.nome
 
     const id_lobo = document.querySelector(".id_lobo")
+
     id_lobo.textContent = "ID:" + lobo.id
 
     let imagem_lobo = document.querySelector('.img_adotar_lobo')
