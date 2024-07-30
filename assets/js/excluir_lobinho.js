aafadf9676e3fd5474ff9989916515a5a3f89aa6
LuccaACV <lucca.valente@gmail.com>
2024-07-30 14:24:56 -0300

    feat: apos excluir volta a lista_lobinhos

--- assets/js/excluir_lobinho.js
+++ assets/js/excluir_lobinho.js
@@ -4,4 +4,5 @@ function enviarDados() {
     let lobos_lista = JSON.parse(localStorage.getItem('lobos'))
     lobos_lista[lobo.id - 1] = null
     localStorage.setItem('lobos', JSON.stringify(lobos_lista))
+    window.location.href = "lista_lobinho.html";
 }
